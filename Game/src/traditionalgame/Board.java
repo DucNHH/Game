b526757b153d2e583154a83a094bad6709565451
DucNHH <85165964+DucNHH@users.noreply.github.com>
2021-06-07 10:38:44 +0700

    ô ăn quan

--- Game/src/traditionalgame/Board.java
+++ Game/src/traditionalgame/Board.java
@@ -1,64 +1,99 @@
 package traditionalgame;
 
-public class Board {
+public class Board{
     int[][] bText;
-    int[] count = new int[2];
+    int[] count;
+    int x, y;
     boolean player;
     int eatScore1, eatScore2;
+
+    public Board() {
+        this.bText = new int[2][6];
+        this.count = new int[2];
+        this.player = true;
+        this.eatScore1 = 0;
+        this.eatScore2 = 0;
+        for(int i = 0; i < 5; ++i) {
+            this.bText[0][i] = 5;
+            this.bText[1][i] = 5;
+        }
+        this.bText[0][5] = 10;
+        this.bText[1][5] = 10;
+    }
     
+    public Board clone() {
+        Board tmp = new Board();
+        tmp.bText = this.bText.clone();
+        for(int i = 0; i < 2; ++i) {
+            tmp.bText[i] = this.bText[i].clone();
+        }
+        tmp.count = this.count.clone();
+        tmp.player = this.player;
+        tmp.eatScore1 = this.eatScore1;
+        tmp.eatScore2 = this.eatScore2;
+        tmp.x = this.x;
+        tmp.y = this.y;
+        return tmp;
+    }
     
-    
-    public void moveLeft(int i, int j) {
+    public void move1() {
+        int i = this.x;
+        int j = this.y;
         do{
-            int tmp = bText[i][j];
-            bText[i][j] = 0;
+            int tmp = this.bText[i][j];
+            this.bText[i][j] = 0;
             while(tmp > 0) {
                 while(tmp > 0 && j > 0) {
                     --j;
-                    ++bText[i][j];
+                    ++this.bText[i][j];
                     --tmp;
                 }
                 if(tmp > 0) {
                     j = 5;
                     i = 1 - i;
-                    ++bText[i][j];
-                    ++count[i];
+                    ++this.bText[i][j];
+                    ++this.count[i];
                     --tmp;
                 }
             }
             if(j == 0) break;
             --j;
-        } while(bText[i][j] != 0);
-        while(bText[i][j] == 0 && j != 5) {
+        } while(this.bText[i][j] != 0);
+        while(this.bText[i][j] == 0 && j != 5) {
             if(j == 0) {
-                if(bText[1-i][5] != 0) {
-                    if(count[i] < 4) break;
+                if(this.bText[1-i][5] != 0) {
+                    if(this.count[1-i] < 4) break;
                     i = 1 - i;
                     j = 6;
-                    count[i] = 0;
+                    this.count[i] = 0;
                 }
+                else break;
             }
             --j;
-            if(player) eatScore1 += bText[i][j];
-            else eatScore2 += bText[i][j];
+            if(this.bText[i][j] == 0) break;
+            if(this.player) this.eatScore1 += this.bText[i][j];
+            else this.eatScore2 += this.bText[i][j];
+            this.bText[i][j] = 0;
             if(j > 0) --j;
             else break;
         }
     }
     
-    public void moveRight(int i, int j) {
+    public void move2() {
+        int i = this.x;
+        int j = this.y;
         do{
-            int tmp = bText[i][j];
-            bText[i][j] = 0;
+            int tmp = this.bText[i][j];
+            this.bText[i][j] = 0;
             while(tmp > 0) {
                 while(tmp > 0 && j < 4) {
                     ++j;
-                    ++bText[i][j];
+                    ++this.bText[i][j];
                     --tmp;
                 }
                 if(tmp > 0) {
-                    ++bText[i][5];
-                    ++count[i];
+                    ++this.bText[i][5];
+                    ++this.count[i];
                     j = -1;
                     i = 1 - i;
                     --tmp;
@@ -66,19 +101,80 @@ public class Board {
             }
             if(j == 4) break;
             ++j;
-        } while(bText[i][j] != 0);
-        while(bText[i][j] == 0 && j != 5 && bText[i][++j] != 0) {
-            ++j;
-            int tmp = bText[i][j];
+        } while(this.bText[i][j] != 0);
+        while(this.bText[i][j] == 0 && j != 5 && this.bText[i][++j] != 0) {
+            int tmp = this.bText[i][j];
+            this.bText[i][j] = 0;
             if(j == 5) {
-                if(count[i] < 4) break;
-                count[i] = 0;
+                if(this.count[i] < 4) {
+                    this.bText[i][j] = tmp;
+                    break;
+                }
+                this.count[i] = 0;
                 i = 1 - i;
                 j = -1;
             }
-            if(player) eatScore1 += tmp;
-            else eatScore2 += tmp;
+            if(this.player) this.eatScore1 += tmp;
+            else this.eatScore2 += tmp;
             ++j;
         }
+    }
+    
+    public int update() {
+        this.player = !this.player;
+        int score1 = this.eatScore1;
+        int score2 = this.eatScore2;
+        for(int i = 0; i < 5; ++i) {
+            score1 += this.bText[0][i];
+            score2 += this.bText[1][i];
+        }
+        if(score1 == 0 && this.player) return 2;
+        if(score2 == 0 && !this.player) return 1;
+        if(this.bText[0][5] == 0 && this.bText[1][5] == 0) {
+            if(score1 > score2) return 1;
+            if(score2 > score1) return 2;
+            return 0;
+        }
+        if(this.player) {
+            int i = 0;
+            while(i < 5 && this.bText[0][i] == 0) {
+                ++i;
+            }
+            if(i == 5) {
+                while(this.eatScore1 > 0 && i > 0) {
+                    --i;
+                    this.bText[0][i] = 1;
+                    --this.eatScore1;
+                }
+            }
+        }
+        else {
+            int i = 0;
+            while(i < 5 && this.bText[1][i] == 0) {
+                ++i;
+            }
+            if(i == 5) {
+                while(this.eatScore2 > 0 && i > 0) {
+                    --i;
+                    this.bText[1][i] = 1;
+                    --this.eatScore2;
+                }
+            }            
+        }
+        return -1;
+    }
+    
+    public void moveLeft() {
+        int i = this.x;
+        int j = this.y;
+        if(this.player) this.move1();
+        else this.move2();
+    }
+    
+    public void moveRight() {
+        int i = this.x;
+        int j = this.y;
+        if(!this.player) this.move1();
+        else this.move2();
     }
 }
--- Game/src/traditionalgame/Controller2.java
+++
@@ -1,9 +0,0 @@
-package traditionalgame;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-
-public class Controller2 {
-    @FXML Button b_0_0, b_0_1, b_0_2, b_0_3, b_0_4, b_0_5, b_1_0, b_1_1, b_1_2, b_1_3, b_1_4, b_1_5;
-    
-}
